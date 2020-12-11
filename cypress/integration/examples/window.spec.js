27c2ef9c5020514539e94fed24ad07a876042a04
Diogo Mariano <diogo.mariano@hitachivantara.com>
2020-12-11 12:11:43 +0000

    remove dumb tests and add cy scripts

--- cypress/integration/examples/window.spec.js
+++ cypress/integration/examples/window.spec.js
@@ -2,21 +2,11 @@
 
 context('Window', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/window')
-  })
-
-  it('cy.window() - get the global window object', () => {
-    // https://on.cypress.io/window
-    cy.window().should('have.property', 'top')
-  })
-
-  it('cy.document() - get the document object', () => {
-    // https://on.cypress.io/document
-    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy.visit('https://lumada-design.github.io/app/master/')
   })
 
   it('cy.title() - get the title', () => {
     // https://on.cypress.io/title
-    cy.title().should('include', 'Kitchen Sink')
+    cy.title().should('include', 'UIKit React setup')
   })
 })
