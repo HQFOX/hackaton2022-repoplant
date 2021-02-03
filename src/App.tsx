87d0703582f651116837486379bf2cdd127fd147
Francisco Guilherme <francisco.guilherme@hitachivantara.com>
2021-02-03 11:26:04 +0000

    chore: route based lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React, { Suspense } from "react";
-import { Router } from "react-router-dom";
+import React from "react";
 import GlobalProvider from "lib/providers/GlobalProvider";
 import DataProvider from "lib/providers/DataProvider";
-import history from "lib/utils/history";
 import Routes from "lib/routes";
 import "lib/i18n";
 
 const App: React.FC = () => (
   <GlobalProvider>
     <DataProvider>
-      <Router history={history}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes />
-        </Suspense>
-      </Router>
+      <Routes />
     </DataProvider>
   </GlobalProvider>
 );
--- src/lib/routes/index.tsx
+++ src/lib/routes/index.tsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Login from "pages/Login";
-import Overview from "pages/Overview";
-import Characters from "pages/Characters";
-import CharacterDetails from "pages/CharacterDetails";
-import Planets from "pages/Planets";
+import React, { Suspense } from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import { lazy } from "@loadable/component";
 import NotFound from "pages/NotFound";
+import history from "lib/utils/history";
 import AuthRoute from "./AuthRoute";
 
+const Login = lazy(() => import("pages/Login"));
+const Overview = lazy(() => import("pages/Overview"));
+const Characters = lazy(() => import("pages/Characters"));
+const CharacterDetails = lazy(() => import("pages/CharacterDetails"));
+const Planets = lazy(() => import("pages/Planets"));
+
 const Routes = () => (
-  <Switch>
-    <AuthRoute path="/" exact redirect="/overview" />
-    <AuthRoute path="/overview" component={Overview} />
-    <AuthRoute path="/login" exact component={Login} />
-    <AuthRoute path="/star-wars" exact redirect="/star-wars/home" />
-    <AuthRoute path="/star-wars/characters" exact component={Characters} />
-    <AuthRoute
-      path="/star-wars/characters/:id"
-      exact
-      component={CharacterDetails}
-    />
-    <AuthRoute path="/star-wars/planets" exact component={Planets} />
-    <Route component={NotFound} />
-  </Switch>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Router history={history}>
+      <Switch>
+        <AuthRoute path="/" exact redirect="/overview" />
+        <AuthRoute path="/overview" component={Overview} />
+        <AuthRoute path="/login" exact component={Login} />
+        <AuthRoute path="/star-wars" exact redirect="/star-wars/home" />
+        <AuthRoute path="/star-wars/characters" exact component={Characters} />
+        <AuthRoute
+          path="/star-wars/characters/:id"
+          exact
+          component={CharacterDetails}
+        />
+        <AuthRoute path="/star-wars/planets" exact component={Planets} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </Suspense>
 );
 
 export default Routes;
