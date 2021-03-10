21930ce85a7767b265a3c15b6c73043f67f62a92
Julio Costa <julio.costa@hitachivantara.com>
2021-03-10 12:53:44 +0000

    feat(globalactions): import global action component from core. HVUIKIT-5698

--- src/components/common/GlobalActions/GlobalActions.tsx
+++ src/components/common/GlobalActions/GlobalActions.tsx
@@ -1,33 +1,16 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { HvButton, HvTypography } from "@hv/uikit-react-core";
-import { Backwards } from "@hv/uikit-react-icons";
-import { GlobalActionsProps } from "./index";
-import useStyles from "./styles";
 
-const Footer: React.FC<GlobalActionsProps> = ({ name, children }) => {
-  const classes = useStyles();
+import { HvGlobalActions } from "@hv/uikit-react-core";
+
+const Footer = ({ name, children }) => {
   const { goBack } = useHistory();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.background} />
-      <div className={classes.content}>
-        <div>
-          <HvButton icon onClick={goBack}>
-            <Backwards />
-          </HvButton>
-        </div>
-        <HvTypography
-          variant="sectionTitle"
-          component="h1"
-          className={classes.name}
-        >
-          {name}
-        </HvTypography>
-        <div className={classes.actions}>{children}</div>
-      </div>
-    </div>
+    <HvGlobalActions title={name} backButtonAction={goBack}>
+      {children}
+    </HvGlobalActions>
   );
 };
+
 export default Footer;
