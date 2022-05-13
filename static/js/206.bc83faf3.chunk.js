f47cb377076a041e1ecf3e366408795e01b2bbd8
Henrique Raposo <henrique.raposo@hitachivantara.com>
2022-05-13 18:20:25 +0100

    Updates

--- static/js/206.bc83faf3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_hitachivantara_uikit_adoption_ts=self.webpackChunk_hitachivantara_uikit_adoption_ts||[]).push([[206],{86206:function(t,i,n){n.r(i),n.d(i,{default:function(){return r}});n(72791);var a=n(60796),e=n(80184),r=function(){return(0,e.jsx)(a.W2,{children:(0,e.jsx)("h1",{children:"Hello world Features"})})}}}]);
-//# sourceMappingURL=206.bc83faf3.chunk.js.map
